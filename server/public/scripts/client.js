632fabb08dac28e1769a716aba9e387a1705e755
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-08-11 18:27:56 -0500

    updated code used from Saturday group to get to database part

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,89 +1,97 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(document).ready(function () {
+  console.log('JQ');
   // Establish Click Listeners
   setupClickListeners()
   // load existing koalas on page load
   getKoalas();
-
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', sendKoalaToServer);}
-    console.log( 'in addButton on click' );
-  //   // get user input and put in an object
-  //   // NOT WORKING YET :(
-  //   // using a test object
-    // let koalaToSend = {
-    //   name: '',
-    //   age: '',
-    //   gender: '',
-    //   ready: '',
-    //   notes: '',
-    // };
-     // call saveKoala with the new obejct
-  saveKoala( koalaToSend );
+  $('#addButton').on('click', function () {
+    console.log('in addButton on click');
+    // get user input and put in an object
+    // NOT WORKING YET :(
+    // using a test object
+    if ($('#genderIn').val() === '') {
+      alert('Choose Male or Female');
+      return;
+    } else if ($('#readyForTransferIn').val() === '') {
+      alert('Choose True or False');
+      return;
+    } else {
+      let koalaToSend = {
+        name: $('#nameIn').val(),
+        age: $('#ageIn').val(),
+        gender: $('#genderIn').val(),
+        ready: $('#readyForTransferIn').val(),
+        notes: $('#notesIn').val(),
+      };
+      // call saveKoala with the new obejct
+      saveKoala(koalaToSend);
+      emptyInputs();
+    }
+  });
+}
 
-
-function getKoalas(){
-  console.log( 'in getKoalas' );
-   // ajax call to server to get koalas
+function getKoalas() {
+  console.log('in getKoalas');
+  // ajax call to server to get koalas - GET
   $.ajax({
     type: 'GET',
     url: '/koalas'
   }).then(function (response) {
+    console.log(response);
     $('#viewKoalas').empty();
     for (let koala of response) {
-      $('#viewKoalas').append(`
-      <tr>
+      // ID from database for delete button
+      console.log(`${koala.ready}`);
+      if (`${koala.ready}` === 'false') {
+        $('#viewKoalas').append(`
+        <tr>
         <td>${koala.name}</td>
         <td>${koala.age}</td>
         <td>${koala.gender}</td>
         <td>${koala.ready}</td>
         <td>${koala.notes}</td>
+        <td><button>Ready for Transfer</button></td>
+        <td><button>Delete</button></td>
       </tr>
-      `)
+        `);
+      } else {
+        $('#viewKoalas').append(`
+        <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+        <td></td>
+        <td><button>Delete</button></td>
+      </tr>
+        `);
+      }
     }
-  }).catch (function (error){
-    console.log('error');
-    alert('Something Is Wrong');
+  }).catch(function (error) {
+    console.log(error);
+    alert('Something went wrong!')
   })
-  
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-   // ajax call to server to get koalas
-   $.ajax({
-    type: 'POST',
-    url:'/koalas'
-   }).then(function (response){
-    console.log('x',response);
-
-   })
-} // end saveKoala
-
-function sendKoalaToServer () {
-  console.log('in sendKoalaToServer');
+function saveKoala(newKoala) {
+  console.log('in saveKoala', newKoala);
   $.ajax({
     type: 'POST',
     url: '/koalas',
-    data: {
-      name: $('#nameIn').val(),
-      age: $('#ageIn').val(),
-      gender: $('#genderIn').val(),
-      ready: $('#readyForTransferIn').val(),
-      notes: $('#notesIn').val()
-    }
+    data: newKoala
   }).then(function (response) {
+    console.log(response);
     getKoalas();
-  }).catch (function (error){
-    console.log('error');
-    alert('Something Is Wrong');
-  })
+  });
 }
 
-function markReady(){
-  console.log('in markReady');
+function emptyInputs() {
+  $('input').val('');
+  $('select').val('');
 }
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,71 +1,67 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
-// DB CONNECTION
-const currentInventory = [
+const koalas = [
     {
-        id: 1,
         name: 'Scotty',
-        gender: 'M',
         age: 4,
-        ready: 'Yes',
+        gender: 'M',
+        ready: 'true',
         notes: 'Born in Guatemala'
     },
     {
-        id: 2,
         name: 'Jean',
-        gender: 'F',
         age: 5,
-        ready: 'Yes',
+        gender: 'F',
+        ready: 'true',
         notes: 'Allergic to lots of lava'
     },
     {
-        id: 3,
         name: 'Ororo',
-        gender: 'F',
         age: 7,
-        ready: 'No',
+        gender: 'F',
+        ready: 'false',
         notes: 'Loves listening to Paula (Abdul)'
     },
     {
-        id: 4,
         name: 'Logan',
-        gender: 'M',
         age: 15,
-        ready: 'No',
+        gender: 'M',
+        ready: 'false',
         notes: 'Loves the sauna'
     },
     {
-        id: 5,
         name: 'Charlie',
-        gender: 'M',
         age: 9,
-        ready: 'Yes',
+        gender: 'M',
+        ready: 'true',
         notes: 'Favorite band is Nirvana'
     },
     {
-        id: 6,
         name: 'Betsy',
-        gender: 'F',
         age: 4,
-        ready: 'Yes',
+        gender: 'F',
+        ready: 'true',
         notes: 'Has a pet iguana'
     }  
-];
+  ];
+
+// DB CONNECTION
+
 
 // GET
-koalaRouter.get('/', (req, res) =>{
-    res.send(currentInventory);
-    console.log(currentInventory);
-});
+koalaRouter.get('/', (req, res) => {
+    res.send(koalas);
+})
 
 // POST
 koalaRouter.post('/', (req, res) => {
     const koala = req.body;
-    console.log('koala',koala);
-    currentInventory.push(koala);
-    res.sendStatus(201);
-  });
+    console.log(req.body);
+    koalas.push(koala);
+    res.send(koala);
+    // res.sendStatus(201);
+  })
 
 // PUT
 
